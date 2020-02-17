9402d5edcf389d0d466f9299436d0d4f495ca20a
Thomas <tom.ericdelmas@gmail.com>
2020-02-17 17:23:24 +0100

    push initial vue nativescript app

---
+++ app/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <Page>
+        <ActionBar title="Plan your murder !"/>
+        <ScrollView>
+            <StackLayout class="home-panel">
+                <!--Add your page content here-->
+               <TextField v-model = "textFieldValue" 
+                          hint = "Entrez votre code" 
+                          maxLength = "6" 
+                          keyboardType = "number"
+                          fontSize = "40"
+                          textAlignment = "center"/>
+                <Button text = "Valider" 
+                        @tap = "buttonTapped(textFieldValue)"
+                        fontSize = "20"/>
+            </StackLayout>
+        </ScrollView>
+    </Page>
+</template>
+
+<script>
+export default {
+    methods : {
+        buttonTapped(input) {
+            //vérifier si input == code de partie reçu par l'API
+            console.log(input);
+        }
+    },
+    data () {
+        return {
+            textFieldValue:""
+        };
+    },
+}
+</script>
+
+<style scoped>
+.home-panel {
+    vertical-align: center;
+    font-size: 20;
+    margin: 15;
+}
+
+.description-label {
+    margin-bottom: 15;
+}
+</style>
