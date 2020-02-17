8676be4957082416d9cf9774fc83f920f43b778a
Thomas <tom.ericdelmas@gmail.com>
2020-02-17 21:07:56 +0100

    HTTP GET request works !

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,47 +1,58 @@
 <template>
-    <Page>
-        <ActionBar title="Plan your murder !"/>
-        <ScrollView>
-            <StackLayout class="home-panel">
-                <!--Add your page content here-->
-               <TextField v-model = "textFieldValue" 
-                          hint = "Entrez votre code" 
-                          maxLength = "6" 
-                          keyboardType = "number"
-                          fontSize = "40"
-                          textAlignment = "center"/>
-                <Button text = "Valider" 
-                        @tap = "buttonTapped(textFieldValue)"
-                        fontSize = "20"/>
-            </StackLayout>
-        </ScrollView>
-    </Page>
+  <Page>
+    <ActionBar title="Plan your murder !" />
+    <ScrollView>
+      <StackLayout class="home-panel">
+        <!--Add your page content here-->
+        <TextField
+          v-model="textFieldValue"
+          hint="Entrez votre code"
+          maxlength="6"
+          keyboardType="number"
+          fontSize="40"
+          textAlignment="center"
+        />
+        <Button text="Valider" @tap="buttonTapped(textFieldValue)" fontSize="20" />
+      </StackLayout>
+    </ScrollView>
+  </Page>
 </template>
 
 <script>
+import * as http from "http";
 export default {
-    methods : {
-        buttonTapped(input) {
-            //vérifier si input == code de partie reçu par l'API
-            console.log(input);
-        }
+  methods: {
+    buttonTapped(input) {
+      //GET
+      http
+        .getJSON("https://rpplanner-api.herokuapp.com/api/games/" + input)
+        .then(
+          result => {
+            this.gameJson = result;
+            console.log(JSON.stringify(this.gameJson));
+          },
+          error => {
+            console.log(error);
+          }
+        );
     },
-    data () {
-        return {
-            textFieldValue:""
-        };
-    },
-}
+    data() {
+      return {
+        textFieldValue: ""
+      };
+    }
+  }
+};
 </script>
 
 <style scoped>
 .home-panel {
-    vertical-align: center;
-    font-size: 20;
-    margin: 15;
+  vertical-align: center;
+  font-size: 20;
+  margin: 15;
 }
 
 .description-label {
-    margin-bottom: 15;
+  margin-bottom: 15;
 }
 </style>
